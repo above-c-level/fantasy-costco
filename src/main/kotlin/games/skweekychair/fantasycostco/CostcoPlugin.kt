1b3d41e0ce4ea39d9b6da9235ef21d75eb648f43
MrMyastan <53984236+MrMyastan@users.noreply.github.com>
2022-01-08 17:11:50 -0600

    lil start

---
+++ src/main/kotlin/games/skweekychair/fantasycostco/CostcoPlugin.kt
@@ -0,0 +1,7 @@
+package games.skweekychair.fantasycostco
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CostcoPlugi: JavaPlugin() {
+
+}
--- src/main/kotlin/games/skweekychair/fantasycostco/test.kt
+++
@@ -1,5 +0,0 @@
-package games.skweekychair.fantasycostco
-
-fun main() {                        // 2
-    println("Hello, World!")        // 3
-}
