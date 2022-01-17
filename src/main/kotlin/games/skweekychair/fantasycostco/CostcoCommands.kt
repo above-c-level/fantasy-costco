c1807aaca95a8360759cd21c6cb7e67f7b75869f
MrMyastan <53984236+MrMyastan@users.noreply.github.com>
2022-01-16 20:08:31 -0600

    more changes

--- src/main/kotlin/games/skweekychair/fantasycostco/CostcoCommands.kt
+++ src/main/kotlin/games/skweekychair/fantasycostco/CostcoCommands.kt
@@ -3,10 +3,24 @@ package games.skweekychair.fantasycostco
 import org.bukkit.command.TabExecutor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+import org.bukkit.ChatColor
 
 object SellCommand : TabExecutor {
 
-    override fun onCommand(sender: CommandSender, cmd: Command, lbl: String, args: Array<String>): Boolean {return false}
+    override fun onCommand(sender: CommandSender, cmd: Command, lbl: String, args: Array<String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage(ChatColor.RED.char + "You have to be a player to use this command.")
+        }
+
+        val player = sender as Player
+
+        val item = player.inventory.itemInMainHand.type.name
+        sender.sendMessage("$item")
+        
+        return true
+    }
 
     override fun onTabComplete(sender: CommandSender, cmd: Command, lbl: String, args: Array<String>): List<String> {return listOf<String>()}
 }
